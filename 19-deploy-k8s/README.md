af4902cdc15a861eb9fdb9974343905818c676e0
Wander Maia <wander.silva@semeartech.com.br>
2024-06-16 08:42:40 -0300

    Inicializado módulo

--- 19-deploy-k8s/README.md
+++ 19-deploy-k8s/README.md
@@ -1,3 +1,9 @@
 # Anotçãoes Aulas
 
+Inicializando o módulo
 
+```bash
+
+go mod init github.com/wandermaia/aulas-pos-golang/tree/main/19-deploy-k8s
+
+```
--- 19-deploy-k8s/main.go
+++ 19-deploy-k8s/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
+	log.Printf("Iniciando Server")
 	http.ListenAndServe(":8080", nil)
 }
